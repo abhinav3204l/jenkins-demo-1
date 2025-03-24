55b6413c83f57cac9cd306354a239608063ebc23
abhinav3204l <245122748014@mvsrec.edu.in>
2025-03-24 12:55:22 +0530

    Add files via upload

---
+++ arguments.java
@@ -0,0 +1,14 @@
+class arguments
+{
+public static void ain(String args[])
+{
+String s1=args[0];
+String s2=args[1];
+String s3=args[2];
+
+System.out.println("name"+s1);
+System.out.println("rno"+s2);
+System.out.println("dept"+s3);
+
+}
+}
